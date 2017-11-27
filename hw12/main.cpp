4b9df70bf788dc8a3f70a431ee7aa533c2bce398
Brian Clinkenbeard <brianclinkenbeard@users.noreply.github.com>
2017-11-27 12:48:13 -0800

    print mst

--- hw12/main.cpp
+++ hw12/main.cpp
@@ -85,15 +85,14 @@ void Graph::mst(std::string src)
         key[it->first] = INF;
     }
 
-    // To store parent array which in turn store MST
-    //std::map<std::string, std::string> parent;
-    std::map<std::string, std::vector<std::string>> parent;
+    // to store parent array of MST
+    std::map<std::string, std::string> parent;
+    //std::map<std::string, std::vector<std::string>> parent;
     for (auto it = adj.begin(); it != adj.end(); it++) {
-        //parent[it->first] = "unitialized";
-        parent[it->first].push_back("unitialized");
+        parent[it->first] = "UNITIALIZED";
     }
 
-    // To keep track of vertices included in MST
+    // keep track of vertices within MST
     std::map<std::string, bool> inMST;
     for (auto it = adj.begin(); it != adj.end(); it++) {
         inMST[it->first] = false;
@@ -120,94 +119,31 @@ void Graph::mst(std::string src)
             if (inMST[v] == false && key[v] > weight) {
                 key[v] = weight;
                 pq.push(make_pair(key[v], v));
-                //parent[v] = u;
-                parent[v].push_back(u);
+                parent[v] = u;
             }
         }
     }
 
     // print MST
     std::cout << "Finding MST using Prim's algorithm..." << std::endl;
-    /*
-    for (auto i = parent.begin(); i != parent.end(); ++i) {
-        std::cout << "Key: " << i->first << std::endl;
-        std::cout << "Parent: " << i->second << std::endl;
-    }
-    */
-    auto end = parent.end();
-    end--;
-    for (unsigned int i = 0; i < end->second.size(); ++i) {
-        std::cout << end->second.at(i) << std::endl;
-    }
-}
 
-/*
-// Prints shortest paths from src to all other vertices
-void Graph::primMST()
-{
-    // Create a priority queue to store vertices that
-    // are being preinMST. This is weird syntax in C++.
-    // Refer below link for details of this syntax
-    // http://geeksquiz.com/implement-min-heap-using-stl/
-    std::priority_queue<isPair, std::vector<isPair>, std::greater<isPair>> pq;
-
-    int src = 0; // Taking vertex 0 as source
-
-    // Create a vector for keys and initialize all
-    // keys as infinite (INF)
-    vector<int> key(V, INF);
-
-    // To store parent array which in turn store MST
-    vector<int> parent(V, -1);
-
-    // To keep track of vertices included in MST
-    vector<bool> inMST(V, false);
-
-    // Insert source itself in priority queue and initialize
-    // its key as 0.
-    pq.push(make_pair(0, src));
-    key[src] = 0;
-
-    // Looping till priority queue becomes empty
-    while (!pq.empty())
-    {
-        // The first vertex in pair is the minimum key
-        // vertex, extract it from priority queue.
-        // vertex label is stored in second of pair (it
-        // has to be done this way to keep the vertices
-        // sorted key (key must be first item
-        // in pair)
-        int u = pq.top().second;
-        pq.pop();
-
-        inMST[u] = true;  // Include vertex in MST
-
-        // 'i' is used to get all adjacent vertices of a vertex
-        list< pair<int, int> >::iterator i;
-        for (i = adj[u].begin(); i != adj[u].end(); ++i)
-        {
-            // Get vertex label and weight of current adjacent
-            // of u.
-            int v = (*i).first;
-            int weight = (*i).second;
-
-            //  If v is not in MST and weight of (u,v) is smaller
-            // than current key of v
-            if (inMST[v] == false && key[v] > weight)
-            {
-                // Updating key of v
-                key[v] = weight;
-                pq.push(make_pair(key[v], v));
-                parent[v] = u;
+    int mst_total = 0;
+    for (auto i = std::next(parent.begin()); i != parent.end(); i++) {
+        // find edge
+        int edge = -1;
+        for (auto j = adj[i->first].begin(); j != adj[i->first].end(); ++j) {
+            if (j->second == i->second) {
+                edge = j->first;
             }
         }
+
+        std::cout << i->first << " <-> " << i->second << " (" << edge << ")" << std::endl;
+
+        mst_total += edge;
     }
 
-    // Print edges of MST using parent array
-    for (int i = 1; i < V; ++i)
-        printf("%d - %d\n", parent[i], i);
+    std::cout << "Total cost: " << mst_total << std::endl;
 }
-*/
 
 int main()
 {

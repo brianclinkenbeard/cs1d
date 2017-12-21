dbfffa13b3d2ae665fb51ff67aa8a4163746af9f
Brian Clinkenbeard <brianclinkenbeard@users.noreply.github.com>
2017-12-21 00:07:57 -0800

    fix quicksort

--- ec/main.cpp
+++ ec/main.cpp
@@ -1,104 +1,140 @@
 #include <vector>
 #include <iostream>
 #include <iterator>
+#include <algorithm>
 #include <chrono>
 
 enum pivot_type { FIRST, MIDDLE, RANDOM };
-void quicksort(std::vector<int> &elements, pivot_type pv_type, int left, int right)
-{
-    int i = left;
-    int j = right;
-    int pivot, mid;
-    switch (pv_type) {
+
+static int count = 0;
+
+// https://codereview.stackexchange.com/a/77788
+int partition(std::vector<int> &elements, pivot_type pt, const int left, const int right) {
+
+    int pos, pivot;
+    switch (pt) {
     case FIRST:
-        pivot = elements.at(left) + 1;
+        pos = left;
         break;
     case MIDDLE:
-        mid = (left + right) / 2;
-        pivot = elements.at(mid);
+        pos = ((right - left) / 2) + left;
         break;
     case RANDOM:
-        pivot = elements.at(rand() % elements.size());
+        pos = (rand() % (right - left)) + left;
         // fall through
     }
+    pivot = elements.at(pos);
+    count++;
 
-    while(left < j || i < right) {
-        while (elements.at(i) < pivot && i < j) {
-            //std::cout << "i" << i << std::endl;
+    // move the pivot value to the front
+    std::swap(elements.at(pos), elements.at(left));
+    count += 3;
+
+    int i = left + 1;
+    int j = right;
+    while (i <= j) {
+        while(i <= j && elements[i] <= pivot) {
+            count++;
             i++;
         }
-        while (elements.at(j) > pivot && j > i) {
-            //std::cout << "j" << j << std::endl;
+
+        while(i <= j && elements[j] > pivot) {
+            count++;
             j--;
         }
-         if (i <= j) {
-            std::swap(elements.at(i), elements.at(j));
-            i++;
-            j--;
-        }
-        else {
-            if (left < j)
-                quicksort(elements, pv_type, left, j);
 
-            if (i < right)
-                quicksort(elements, pv_type, i, right);
-
-            return;
+        if (i < j) {
+            std::swap(elements[i], elements[j]);
+            count += 3;
         }
     }
+    std::swap(elements[i - 1], elements[left]);
+    count += 3;
+
+    return i - 1;
+}
+
+static int quicksort(std::vector<int> &arr, pivot_type pt, const int left, const int right) {
+    count++;
+    if (left >= right) {
+        return count;
+    }
+
+    int part = partition(arr, pt, left, right);
+
+    quicksort(arr, pt, left, part - 1);
+    quicksort(arr, pt, part + 1, right);
 }
 
 int main()
 {
-    for (int i = 0; i < 3; ++i) {
-        std::vector<int> example;
-        switch (i) {
+    std::cout << "PART ONE:" << std::endl;
+    for (int i = 0; i < 9; ++i) {
+        std::vector<int> base;
+
+        if (i == 0)
+            std::cout << "1. Already ordered:" << std::endl;
+        else if (i == 3)
+            std::cout << "2. Reverse ordered:" << std::endl;
+        else if (i == 6)
+            std::cout << "3. Random ordered:" << std::endl;
+
+        // generate numbers before ordering
+        int size;
+        switch (i % 3) {
         case 0:
-            std::cout << "Using already ordered set and " << std::endl;
-            example = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+            size = 5000;
             break;
         case 1:
-            std::cout << "Using reverse ordered set and " << std::endl;
-            example = { 19, 17, 15, 13, 11, 9, 7, 5, 3, 1 };
+            size = 10000;
             break;
         case 2:
-            std::cout << "Using randomly ordered set and" << std::endl;
-            for (int i = 0; i < 25; ++i)
-                example.push_back(rand() % 100);
+            size = 50000;
             // fall through
         }
+        std::cout << "Size: " << size << std::endl;
 
+        for (int i = 0; i < size; ++i)
+            base.push_back(rand() % 100000);
+
+        if (i < 3)
+            std::sort(base.begin(), base.end());
+        else if (i < 6)
+            std::sort(base.rbegin(), base.rend());
+
+        std::cout << "Before:\t";
+        std::copy(base.begin(), base.begin() + 5, std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
+
+        std::vector<int> vec;
         for (int j = 0; j < 3; ++j) {
             pivot_type type = static_cast<pivot_type>(j);
 
             switch (j) {
             case 0:
-                std::cout << "first element as pivot:" << std::endl;
+                std::cout << "First element as pivot:" << std::endl;
                 break;
             case 1:
-                std::cout << "middle element as pivot:" << std::endl;
+                std::cout << "Middle element as pivot:" << std::endl;
                 break;
             case 2:
-                std::cout << "random element as pivot:" << std::endl;
+                std::cout << "Random element as pivot:" << std::endl;
             }
 
-            std::cout << "Before:\t";
-            std::copy(example.begin(), example.end(), std::ostream_iterator<int>(std::cout, " "));
-            std::cout << std::endl;
+            vec = base;
 
             auto start = std::chrono::system_clock::now();
-            quicksort(example, type, 0, example.size() - 1);
+            // sort
+            std::cout << "Count: " << quicksort(vec, type, 0, vec.size() - 1) << std::endl;
+            // reset counter
+            count = 0;
             auto end = std::chrono::system_clock::now();
 
             std::chrono::duration<double> elapsed_seconds = end - start;
-            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
             std::cout << "elapsed time: " << std::fixed << elapsed_seconds.count() << "s\n";
-
-            std::cout << "After:\t";
-            std::copy(example.begin(), example.end(), std::ostream_iterator<int>(std::cout, " "));
-            std::cout << std::endl << std::endl;
         }
-
+        std::cout << "After:\t";
+        std::copy(vec.begin(), vec.begin() + 5, std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl << std::endl;
     }
 }
